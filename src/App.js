6c74aee352f53d86de97622df07a03c3e0ca606d
shibli21 <syedshiblimahmud@gmail.com>
2020-06-30 20:29:45 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [hour, setHour] = useState(null);
+  const [min, setMin] = useState(null);
+  const [sec, setSec] = useState(null);
+
+  useEffect(() => {
+    date();
+  }, [sec]);
+
+  const date = () => {
+    const deg = 6;
+    let date = new Date();
+
+    const mn = date.getMinutes() * deg;
+    const hr = date.getHours() * 30 + mn / 12;
+    const sc = date.getSeconds() * deg;
+
+    setHour(hr);
+    setMin(mn);
+    setSec(sc);
+  };
+
+  setInterval(date, 1000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="clock">
+      <div className="hour">
+        <div className="hr" style={{ transform: `rotateZ(${hour}deg)` }}></div>
+      </div>
+      <div className="min">
+        <div className="mn" style={{ transform: `rotateZ(${min}deg)` }}></div>
+      </div>
+      <div className="sec">
+        <div className="sc" style={{ transform: `rotateZ(${sec}deg)` }}></div>
+      </div>
     </div>
   );
 }
